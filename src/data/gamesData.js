d89ffef5c0cb560e09048afebb5c2d1f76ad1a78
MarcosFernandesF <marcosrff15@gmail.com>
2025-12-23 22:29:39 -0300

    corrigindo mock

--- src/data/gamesData.js
+++ src/data/gamesData.js
@@ -39,22 +39,22 @@ export const allGames = [
   {
     id: 8,
     title: "Hogwarts Legacy",
-    coverUrl: "https://images.igdb.com/igdb/image/upload/t_cover_big/co59oc.jpg"
+    coverUrl: "https://cdn1.epicgames.com/offer/e97659b501af4e3981d5430dad170911/EGS_HogwartsLegacy_AvalancheSoftware_S2_1200x1600-bb9f789b6628ff2aa935f06f5e0f218a"
   },
   {
     id: 9,
     title: "Starfield",
-    coverUrl: "https://images.igdb.com/igdb/image/upload/t_cover_big/co6d0k.jpg"
+    coverUrl: "https://upload.wikimedia.org/wikipedia/pt/thumb/4/43/Capa_do_jogo_Starfield.jpg/250px-Capa_do_jogo_Starfield.jpg"
   },
   {
     id: 10,
     title: "Final Fantasy XVI",
-    coverUrl: "https://images.igdb.com/igdb/image/upload/t_cover_big/co5x85.jpg"
+    coverUrl: "https://upload.wikimedia.org/wikipedia/en/0/00/Final_Fantasy_XVI_cover_art.png"
   },
   {
     id: 11,
     title: "Resident Evil 4 Remake",
-    coverUrl: "https://images.igdb.com/igdb/image/upload/t_cover_big/co5w0v.jpg"
+    coverUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/d/df/Resident_Evil_4_remake_cover_art.jpg/250px-Resident_Evil_4_remake_cover_art.jpg"
   },
   {
     id: 12,
@@ -64,42 +64,42 @@ export const allGames = [
   {
     id: 13,
     title: "Diablo IV",
-    coverUrl: "https://images.igdb.com/igdb/image/upload/t_cover_big/co69sn.jpg"
+    coverUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/1/1c/Diablo_IV_cover_art.png/250px-Diablo_IV_cover_art.png"
   },
   {
     id: 14,
     title: "Street Fighter 6",
-    coverUrl: "https://images.igdb.com/igdb/image/upload/t_cover_big/co69sk.jpg"
+    coverUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/9/94/Street_Fighter_6_box_art.jpg/250px-Street_Fighter_6_box_art.jpg"
   },
   {
     id: 15,
     title: "Alan Wake 2",
-    coverUrl: "https://images.igdb.com/igdb/image/upload/t_cover_big/co660r.jpg"
+    coverUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/Alan_Wake_2_box_art.jpg/250px-Alan_Wake_2_box_art.jpg"
   },
   {
     id: 16,
     title: "Lies of P",
-    coverUrl: "https://images.igdb.com/igdb/image/upload/t_cover_big/co65u5.jpg"
+    coverUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/d/de/Lies_of_p_cover_art.jpg/250px-Lies_of_p_cover_art.jpg"
   },
   {
     id: 17,
     title: "Armored Core VI: Fires of Rubicon",
-    coverUrl: "https://images.igdb.com/igdb/image/upload/t_cover_big/co6617.jpg"
+    coverUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/8/89/Armored_Core_VI_Fires_of_Rubicon_cover_art.jpg/250px-Armored_Core_VI_Fires_of_Rubicon_cover_art.jpg"
   },
   {
     id: 18,
     title: "Mortal Kombat 1",
-    coverUrl: "https://images.igdb.com/igdb/image/upload/t_cover_big/co68ke.jpg"
+    coverUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/5/5b/Mortal_Kombat_1_key_art.jpeg/250px-Mortal_Kombat_1_key_art.jpeg"
   },
   {
     id: 19,
     title: "Assassin's Creed Mirage",
-    coverUrl: "https://images.igdb.com/igdb/image/upload/t_cover_big/co660t.jpg"
+    coverUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/2/23/Assassin%27s_Creed_Mirage_cover.jpeg/250px-Assassin%27s_Creed_Mirage_cover.jpeg"
   },
   {
     id: 20,
     title: "Payday 3",
-    coverUrl: "https://images.igdb.com/igdb/image/upload/t_cover_big/co68f9.jpg"
+    coverUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/7/7c/Payday_3_cover_art.jpg/250px-Payday_3_cover_art.jpg"
   }
 ];
 
